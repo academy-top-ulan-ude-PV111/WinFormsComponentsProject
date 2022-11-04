974fa0c2cb9d9ddd38884f82345193d2508c15b1
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-04 16:11:46 +0300

    04-11-22

--- WinFormsComponentsProject/Form1.Designer.cs
+++ WinFormsComponentsProject/Form1.Designer.cs
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(245, 43);
+            this.button1.Location = new System.Drawing.Point(243, 290);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(279, 84);
             this.button1.TabIndex = 0;
@@ -42,20 +48,61 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(198, 103);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(371, 36);
+            this.txtName.TabIndex = 1;
+            this.txtName.Leave += new System.EventHandler(this.txtName_Leave);
+            this.txtName.Validating += new System.ComponentModel.CancelEventHandler(this.txtName_Validating);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAge.Location = new System.Drawing.Point(198, 197);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(371, 36);
+            this.txtAge.TabIndex = 1;
+            this.txtAge.Validating += new System.ComponentModel.CancelEventHandler(this.txtAge_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private TextBox txtName;
+        private ErrorProvider errorProvider1;
+        private TextBox txtAge;
+        private Label label1;
     }
 }
--- WinFormsComponentsProject/Form1.cs
+++ WinFormsComponentsProject/Form1.cs
@@ -11,10 +11,43 @@ namespace WinFormsComponentsProject
         {
             string title = "Message Box Title";
             string message = "Back paint red?";
-            DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.YesNo);
+            DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (result == DialogResult.Yes)
                 this.BackColor = Color.Red;
 
+        }
+
+        private void txtName_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (txtName.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtName, "Имя пустое!");
+                button1.Enabled = false;
+            }
+                
+            else if (txtName.Text.Length < 3)
+                errorProvider1.SetError(txtName, "Имя короткое!");
+            else
+                errorProvider1.Clear();
+        }
+
+        private void txtAge_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            int age = 0;
+            if (txtAge.Text.Trim() == "")
+                errorProvider1.SetError(txtAge, "Возраст не задан!");
+            else if (!Int32.TryParse(txtAge.Text, out age))
+                errorProvider1.SetError(txtAge, "Возраст задан неправильно!");
+            else
+                errorProvider1.Clear();
+        }
+
+        private void txtName_Leave(object sender, EventArgs e)
+        {
+            if (txtName.Text.Trim() == "") 
+            {
+                
+            }
         }
     }
 }
